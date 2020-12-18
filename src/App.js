f4094f20cfc840083b7feef193ebfc828f07cfd0
iwinbae <eivindnguyen11@gmail.com>
2020-12-18 22:06:41 +0100

    initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import familypic from "./images/olood_gang1.JPG";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Gang from "./Gang";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <Link to="/home">OLOOD.</Link>
+        </header>
+
+        <nav className="nav-bar">
+          <Link to="/connect"> CONNECT </Link>/<Link to="/gang"> GANG </Link>/
+          <Link to="/merch"> MERCH </Link>/<Link to="/work"> WORK </Link>
+        </nav>
+
+        <main>
+          <Switch>
+            {/*{" "}
+            <Route path="/connect">
+              <Connect />
+            </Route>{" "}
+  */}
+            <Route path="/home">
+              <Home />
+            </Route>
+
+            <Route path="/gang">
+              <Gang />
+            </Route>
+            {/*{" "}
+            <Route path="/merch">
+              <Merch />
+            </Route>
+            <Route path="/work">
+              <Work />
+            </Route>{" "}
+*/}
+          </Switch>
+        </main>
+      </Router>
+
+      <footer>
+        <p> © Copyright All Rights Reserved 2020 OLOOD Corp. ™</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+function Home() {
+  return <img src={familypic}></img>;
+}
---
+++ src/Gang.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import duy from "./images/duy.png";
+import an from "./images/an.png";
+import fok from "./images/fok.png";
+import bok from "./images/bok.png";
+import king from "./images/king.png";
+
+function Members({ name, pic }) {
+  return (
+    <div className="gang-box">
+      <p className="name">{name}</p>
+      <img className="member-pic" src={pic}></img>
+    </div>
+  );
+}
+
+function Gang() {
+  return (
+    <div className="gang-box">
+      <Members name="Duy" pic={duy} />
+      <Members name="An" pic={an} />
+      <Members name="Foko" pic={fok} />
+      <Members name="Boko" pic={bok} />
+      <Members name="King" pic={king} />
+    </div>
+  );
+}
+
+export default Gang;
